028d92d643ef692b29eb0a048429f570d20bf909
darkfe <i@darkfe.com>
2013-07-21 09:14:37 +0800

    v0.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-tinyswitch
+TinySwitch
 ==========
 
-tinyswitch 是一个无聊的低性能的jquery插件,主要用来解决这种场景下的问题:
+TinySwitch 是一个无聊的低性能的jquery插件,主要用来解决这种场景下的问题:
 
 1. 点击 radio A, 隐藏一片区域, 点击radio B, 隐藏这篇区域
 2. 勾选 checkbox, 启用一部分控件,  取消勾选 checkbox, 禁用一部分控件
@@ -9,32 +9,217 @@ tinyswitch 是一个无聊的低性能的jquery插件,主要用来解决这种�
 
 这种无聊并且重复的逻辑大量充斥在各种表单中.
 
-tinyswitch 提供了这样的一种方案来试图简化(或者复杂化?)这个鸟玩意儿.
+TinySwitch 提供了这样的一种方案来试图简化(或者复杂化?)这个鸟玩意儿.
 
 你只需要在控件上配置:
 
 ```
-data-tinyswitch=".selector@action1:action2"
+data-tinyswitch="selector@action"
 ```
 
 就可以操控对应的内容展示隐藏,或者启用禁用,以及其他一些别的什么逻辑.
 
-
-* `.selector` 为标准的jQuery支持的css选择器, 它告诉`tinyswitch`, 谁是点击后的操作对象
+* `selector` 为标准的jQuery支持的css选择器, 它告诉`TinySwitch`, 谁是点击后的操作对象
 * `@` @是个分界符,表示后面的部分是`行为`
-* `action1` 表示`选中时`执行这个行为
-* `:` :分界符
-* `action2` 表示`非选中时`执行这个行为, 非选中时的行为是可选的.
+* `action` 表示`选中时`执行这个行为 
 
-目前支持的默认行为有:
 
-1. show
-2. hide
-3. disabled 或者 off
-4. enabled 或者 on
-5. focus
+对于不同的控件, `TinySwitch`提供了三种配置方式来满足需求:
 
-你可以通过脚本来增加自定义的行为
+### 单选框
+
+由于单选框自己不会主动触发`取消选中`这个行为, 所以单选框只需要配置`选中`行为, 例如:
+
+```html
+<label>
+  <input type="radio" data-tinyswitch="#box@show"> 显示box
+</label>
+
+<label>
+  <input type="radio" data-tinyswitch="#box@hide"> 隐藏box
+</label>
+
+<div id="box">我是box</div>
+```
+
+### 复选框
+
+复选框本身会触发两个行为: `勾选` 和 `取消勾选`, 所以复选框可以配置两个`行为`, 例如:
+
+
+```html
+<label>
+  <input type="checkbox" data-tinyswitch="#box@show:hide, #list@show:hide"> 显示或者隐藏box
+</label> 
+
+<div id="box">我是box</div>
+<div id="list">list</div>
+```
+
+### 下拉列表
+
+下拉列表同时拥有两种配置方式, 一种与单选框一致:
+
+```html
+<select> 
+  <option value="1" data-tinyswitch="#box@show">value is 1</option>
+  <option value="2" data-tinyswitch="#box@hide">value is 2</option>
+</select>
+
+<div id="box">我是box</div>
+```
+
+但如果一个`select`中只有一个选项需要增加行为, 其他选项与该选项行为互斥的话,这样配置方法就太过繁琐, 可以使用另一种方式
+
+```html
+<select data-tinyswitch="#box@show(value==1)&hide(value!=1)"> 
+  <option value="1">value is 1</option>
+  <option value="2">value is 2</option>
+</select>
+
+<div id="box">我是box</div>
+```
+
+`#box@show(value==1)&hide(value!=1)` 表示:
+
+当`select`的`value == 1`的时候,执行`show`这个行为
+当`select`的`value != 1`的时候,执行`hide`这个行为 
+
+`(value==1)` 表达式中的`value`关键字表示当前`select`的`value`值
+
+`TinySwitch` 还支持`index`关键字,表示当前`select`的`selectIndex`索引
+ 
+
+表达式中目前支持如下运算符:
+
+* `==`  相等
+* `!=`  不等
+* `>`   大于
+* `<`   小于
+* `*=`  包含
+* `$=`  结尾等于
+* `^=`  开头等于
+
+### 更复杂的选择器
+
+`TinySwitch` 可以使用`jQuery`支持的所有`css`选择器来进行全局的元素定位, 比如:
+
+```html
+<label>
+  <input type="checkbox" data-tinyswitch="#box,#list@show:hide"> 显示或者隐藏box 和 list
+</label> 
+
+<div id="box">我是box</div>
+<div id="list">list</div>
+```
+
+
+### 同时使用多个配置
+
+以上的例子中为了简化, 都使用了一个`action`, `TinySwitch`也支持用分号分隔多个`action`按序执行, 例如:
+
+
+```html
+<label>
+  <input type="checkbox" data-tinyswitch="#box@show:hide; #list@show:hide"> 显示或者隐藏box 和 list
+</label> 
+
+<div id="box">我是box</div>
+<div id="list">list</div>
+```
+
+### 多个`action`同时执行
+
+`TinySwitch` 支持多个用`&`隔开的`action`, 按序执行
+
+```html
+<label>
+  <input type="checkbox" data-tinyswitch="#box@show&disabled:hide"> 显示box,并且禁用box中的控件, 或者隐藏box
+</label> 
+
+<div id="box">
+  我是box
+  <input type="text">
+</div> 
+```
+
+### 全局定位和相对定位元素
+
+默认情况下,写在`@`前的`selector`是全局定位的, 即:
+
+`#box@show` 实际上会执行 `$('#box').show()`
+
+但有很多场景中, 我们需要相对于当前控件对目标的容器进行定位, 比如下例:
+
+```html 
+<select data-tinyswitch=".list@enabled(value=='1')&disabled(value$='2')"> 
+  <option value="1">启用</option>
+  <option value="2">禁用</option> 
+</select>  
+<div class="list">
+  <input type="text" value="我是一个文本框">
+</div>
+
+<select data-tinyswitch=".list@enabled(value=='1')&disabled(value$='2')"> 
+  <option value="1">启用</option>
+  <option value="2">禁用</option> 
+</select>
+<div class="list">
+  <input type="text" value="我是一个文本框">
+</div>
+```
+
+我们希望每个`select`都只控制它下面的`.list`,但这样做会导致两个`.list`同时被控制.
+
+针对这种场景 `TinySwitch` 提供了一种简单的标记方式来实现相对于当前控件的定位方式, 语法为:
+
+```
+data-tinyswitch="relativeflag$selector@action"
+```
+
+`relativeflag` 支持如下标记:
+
+`!` 当前控件的`.parent()`
+`+` 当前控件的`.next()`
+`-` 当前控件的`.prev()`
+`^` 当前控件的`.prevAll()`
+`~` 当前控件的`.nextAll()`
+
+对于`!`和`+`和`-`, `TinySwitch`还支持量词:
+
+`2!` 表示`.parent().parent()`
+`3+` 表示`.next().next().next()`
+`1-` 表示`.prev().prev()`
+
+对于上面的例子,我们可以写成这样来实现需求:
+
+```html 
+<select data-tinyswitch="+$.list@enabled(value=='1')&disabled(value$='2')"> 
+  <option value="1">启用</option>
+  <option value="2">禁用</option> 
+</select>
+<div class="list">
+  <input type="text" value="我是一个文本框">
+</div>
+
+<select data-tinyswitch="+$.list@enabled(value=='1')&disabled(value$='2')"> 
+  <option value="1">启用</option>
+  <option value="2">禁用</option> 
+</select>
+<div class="list">
+  <input type="text" value="我是一个文本框">
+</div>
+```
+
+### TinySwitch 默认自带的`action` 
+
+1. `show`   显示这个容器
+2. `hide`   隐藏这个容器
+3. `disabled` 或者 off   禁用这个控件, 或者禁用这个容器中的所有控件
+4. `enabled`  或者 on    启用这个控件, 或者启用这个容器中的所有控件
+5. `focus`    让当前的控件获得焦点
+
+你可以通过脚本来增加自定义的`action`
 
 ```javascript
 $.tinyswitch.addAction('clearValues',function(elements){
@@ -42,46 +227,9 @@ $.tinyswitch.addAction('clearValues',function(elements){
 });
 ```
 
-关于`选中`和`非选中`的定义:
+### TinySwitch 的事件
 
-对于`checkbox`和`radio`来说:
-
-* 当`checked`属性为`true`时, 为`选中`
-* 当`checked`属性为`false`时, 为`非选中`
-
-对于`select`来说:
-
-* 当`selected`属性为`true`时, 为`选中`
-* 当`selected`属性为`false`时, 为`非选中`
-
-对于其他任意元素来说:
-
-* 当`data-tinyswitchvalue="true"` 时,为`选中`
-* 当`data-tinyswitchvalue="true"` 时,为`非选中`
-
-以上展示了基础用法,但`tinyswitch`还支持更多的语法,比如,多串规则
-
-```
-data-tinyswitch=".selector@action1:action2, .selector@action3:action4"
-```
-
-多行为同时执行
-
-```
-data-tinyswitch=".selector@action1&action2:action2&action4"
-```
-
-有些情况下(比如插件中), 无法使用全局选择器来定位目标,为此,`tinyswitch`提供了一种相对于绑定事件的元素定位的机制:
-
-```
-data-tinyswitch=".selector@~action1"
-```
-
-`~` 表示当前元素的父元素中查找`.selector`, `~` 用数量来表示层级, 比如`~~`即表示`.parent().parent.find(".selector")`.
-
-事件支持:
-
-`tinyswitch`目前只提供两个事件:
+`TinySwitch`目前只提供两个事件:
 
 1. `tinyswitch.beforeswitch` 事件, 如果在这个事件中返回`false`, 将停止执行`switch`的操作.
 2. `tinyswitch.afterswitch` 事件, `switch`完成之后触发
--- tinyswitch.js
+++ tinyswitch.js
@@ -1,3 +1,9 @@
+/*!
+ * jQuery TinySwitch Plugin v0.1
+ * Copyright 2013 darkfe(i@darkfe.com)
+ * Released under the MIT license
+ * Date: 2013/7/21
+ */
 
 (function($){
 	/*
@@ -11,64 +17,184 @@
 
 	var HTML_DATA_PREFIX = 'data-tinyswitch'; 
 
-	/*
-		解析 data-tinyswitch="" 属性中的规则
-	*/
-	var parseRule = function(rule){
+	var DEBUG = true;
+
+	var log = function(){
+		if(DEBUG)
+			console.log.apply(console,arguments);
+	}
+
+	var parseRule = function(rule, type){
 		var placeAction = [];
 
-		var parsePattern = /,?\s*([^@]+)\@(~*)([\w\-&]+)(?::(~*)([\w\-&]+))?/ig;
+		var parsePatternForProxyMode = /;?\s*(?:([!^+-~\d]+)\$)?([^@]+)\@((?:\w+(?:\((?:value|index)?[=><!$*^]{1,2}\s*(?:\d+|'(?:\\'|[^'])+')\))?&?)*)/ig;
 
+		var parsePatternSimple = /;?\s*([!^+-~\d]+\$)?([^@]+)\@([\w\-&]+)(?::([~!+\d]*)([\w\-&]+))?/ig;
+
+		var isSelectMode = type === 'selectmode';
+
+		var parsePattern = isSelectMode ?  parsePatternForProxyMode : parsePatternSimple;
+ 
 		var currentRule;
 
 		while(currentRule = parsePattern.exec(rule)){
-
 			placeAction.push({
-				selectedSelector : currentRule[1],
-				selectedRelative : currentRule[2],
+
+				isSelectMode      : isSelectMode,
+				selectedRelative : currentRule[1], 
+				selectedSelector : currentRule[2],
 				selectedAction   : currentRule[3],
 
-				unselectSelector : currentRule[1],
-				unselectRelative : currentRule[4],
+				unselectSelector : currentRule[2],
+				unselectRelative : currentRule[1],
 				unselectAction   : currentRule[5]
 			});
 		}
 
+		log(placeAction);
+
 		return placeAction;
 	}
+ 
 
 	/*
 		执行规则
 	*/
 	var runSwitch = function(placeAction, sender, type){
 		 
-		$.each(placeAction,function(index, item){
+		$.each(placeAction,function(index, item){ 
+
+			/*
+				定义:
+				! 父级    [number]!    0! 表示无   ! 表示 1!
+				+ 当前对象的下一个  [number]+
+				- 当前对象的上一个  [number]-
+				^ 等同当前对象的prevAll()
+				~ 等同当前对象的nextAll()
+			*/
 			var scope = $(document);
 			var parentLevel = 0;
-			if(this.relative){
-				scope = sender; 
-				parentLevel = this.relative.split('~').length;
-				while(parentLevel--){
-					scope = scope.parentNode();
-				}
+			var chars;
+			var exprPattern = /(\d*)([!+^~-])/g;
+			var current;
+			var times = 0;
+			var method = '';
+			if(this[type + 'Relative']){
+				scope = sender;   
+				while(current = exprPattern.exec(this[type + 'Relative'])){ 
+					times = current[1] ? parseInt(current[1],10) : 1;
+
+					switch(current[2]){
+						case '!':  
+							method = 'parent'; 
+						break;
+						case '+':
+							method = 'next'; 
+						break;
+						case '-': 
+							method = 'prev';
+						break;
+						case '~':
+							method = 'nextAll';
+							times = 1;
+						break;
+						case '^':
+							method = 'prevAll';
+							times = 1;
+						break;
+					}
+
+					while(times){
+						scope = scope[method]();
+						times--;
+					}
+				} 
+
+				log('current scope offset:', this[type + 'Relative'], scope);
 			}
 			
-			var actionTypes = this[type + 'Action'];
+			var actionType = this[type + 'Action'];
 
-			if(!actionTypes){
+			if(!actionType){
 				return;
 			}
-			actionTypes = actionTypes.split('&');
 
-			var elements = scope.find(this[type + 'Selector']);
+			var currentType;
+			var actionTypePattern = /(\w+)(?:\((value|index)?([=><!$*^]{1,2})\s*(?:(\d+)|'(\\'|[^']+)'\)))?&?/ig;
+			var actionTypes = [];
 
-			$.each(actionTypes,function(index, type){
-				var currentAction = TinySwitchActions[type];
-				if(currentAction){
-					currentAction.call(elements, elements);
-				}
-			});
-			
+			while(currentType = actionTypePattern.exec(actionType)){
+				actionTypes.push({
+					action : currentType[1],
+					prop   : currentType[2],
+					expr   : currentType[3],
+					value  : currentType[4] || currentType[5]
+				});
+			}
+
+			log('current action types:', actionType, actionTypes);
+
+			//这里选中的元素要包含scope自己,否则 + - 这种就不能工作了
+			var elements = scope.find(this[type + 'Selector']).add(scope.filter(this[type + 'Selector']));
+
+			if(item.isSelectMode){
+				$.each(actionTypes,function(index, typeItem){ 
+					var result = false;
+					var value = $(sender).val();
+					if(typeItem.prop == 'index'){
+						typeItem.value = parseInt(typeItem.value,10) || 0;
+						value = $(sender)[0].selectedIndex;
+					} 
+					/*
+						定义:
+						==	相等
+						!=  不等
+						>   大于
+						<   小于
+						*=  包含
+						$=  结尾等于
+						^=  开头等于
+					*/
+					switch(typeItem.expr){
+						case '==':
+							result = typeItem.value == value;
+						break;
+						case '!=': 
+							result = typeItem.value != value; 
+						break;
+						case '>':
+							result = (parseInt(typeItem.value,10)||0) > (parseInt(value,10)||0);
+						break;
+						case '<':
+							result = (parseInt(typeItem.value,10)||0) < (parseInt(value,10)||0);
+						break;
+						case '*=':
+							result = (value+'').indexOf(typeItem.value) != -1;
+						break;
+						case '$=': 
+							result = (value+'').lastIndexOf(typeItem.value) === (value+'').length - 1;
+						break;
+						case '^=':
+							result = (value+'').indexOf(typeItem.value) === 0;
+						break;
+					}
+
+					if(result){ 
+						var currentAction = TinySwitchActions[typeItem.action];
+						if(currentAction){
+							currentAction.call(elements, elements);
+							return false;
+						}
+					}
+				});
+			}else{
+				$.each(actionTypes,function(index, typeItem){ 
+					var currentAction = TinySwitchActions[typeItem.action];
+					if(currentAction){
+						currentAction.call(elements, elements);
+					}
+				});
+			}
 		});
 	}
 
@@ -110,15 +236,19 @@
 
 		return $(elements).each(function(){
 
-			var eventType = $(this).is('select') ? 'change' : 'click';
+			var target = $(this);
 
-			if($(this).data(JQUERY_DATA_PREFIX + 'init') === true){
+			if(target.data(JQUERY_DATA_PREFIX + 'init') === true){
 				return;
-			}
+			} 
 
-			$(this) 
+			target 
 
-			.data(JQUERY_DATA_PREFIX + 'init',true)
+			.data(JQUERY_DATA_PREFIX + 'init',true); 
+
+			var eventType = target.is('select') ? 'change' : 'click';
+
+			target
 			
 			.on(EVENT_PREFIX + 'switch',function(){
 
@@ -127,17 +257,21 @@
 					($(this).is(':checkbox,:radio') ?
 					($(this).is(':checked') ? 'selected' : 'unselect') :
 					($(this).attr(HTML_DATA_PREFIX + 'value') === 'true' ? 'selected' : 'unselect'));
+
 				var rule = $(this).attr(HTML_DATA_PREFIX);
+				var placeAction;
 
-				if($(this).is('select')){
-					rule = $(this).find('option:selected').attr(HTML_DATA_PREFIX);
-				}
+				if($(this).is('select') && rule){ 
+					placeAction = parseRule(rule, 'selectmode');
+				}else{
+					placeAction = parseRule(rule);
+				} 
 
-				var placeAction = parseRule(rule);
+				log(placeAction)
 
 				runSwitch(placeAction, $(this), actionType); 
 			})
-			
+
 			.on(eventType,function(){
 				if($(this).triggerHandler(EVENT_PREFIX + 'beforeswitch') === false){
 					return;
@@ -149,16 +283,16 @@
 				$(this)
 				.triggerHandler(EVENT_PREFIX + 'afterswitch');
 			})
-			
-			.triggerHandler(EVENT_PREFIX + 'switch');
+
+			.triggerHandler(EVENT_PREFIX + 'switch')
 		});
 	}
 
 	
 
 	//jQuery 插件 
-	$.fn.tinyswitch = function(){
-		return TinySwitch(this);
+	$.fn.tinyswitch = function(options){
+		return TinySwitch(this, options);
 	}; 
 
 	/*
@@ -178,7 +312,7 @@
 
 	//默认加载
 	$(function(){
-		$('['+ HTML_DATA_PREFIX +']:not(option)').each(function(){ 
+		$('['+ HTML_DATA_PREFIX +']:not(option)').each(function(){
 			$(this).tinyswitch();
 		});
 
@@ -192,7 +326,11 @@
 
 		$(document).on('change','select',function(){
 			if($(this).children().is(HTML_DATA_PREFIX) && $(this).data(JQUERY_DATA_PREFIX + 'init') !== true){
-				$(this).tinyswitch();	
+				$(this).tinyswitch();
+			}
+
+			if($(this).is(HTML_DATA_PREFIX)){
+				$(this).tinyswitch();
 			}
 		});
 	});
